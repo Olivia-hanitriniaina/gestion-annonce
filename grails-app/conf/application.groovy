17a92d4f0c9e0e633f452cb537e0e377bd1a0a9c
Antsa <vranari@yahoo.fr>
2021-03-12 15:57:34 +0300

    Update application.groovy

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -5,7 +5,7 @@ grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.mbds.grails.U
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.mbds.grails.UserRole'
 grails.plugin.springsecurity.authority.className = 'com.mbds.grails.Role'
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/**',               access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
 	[pattern: '/index.gsp',      access: ['permitAll']],
@@ -14,6 +14,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
 	[pattern: '/**/favicon.ico', access: ['permitAll']]
 ]
 
